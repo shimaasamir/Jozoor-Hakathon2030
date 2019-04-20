af35e9e3f3fe24d4ed9a85ad2e7f18e83a0dc0ff
AbdelRahmanMSA <abdelrahman.msa@gmail.com>
2019-04-20 02:06:03 +0200

    Adding news feed page

---
+++ clean Metronic/js/controllers/NewsFeedController.js
@@ -0,0 +1,13 @@
+angular.module('JozoorApp').controller('NewsFeedController', function($rootScope, $scope, $http, $timeout,$state) {
+    $scope.$on('$viewContentLoaded', function() {   
+        // initialize core components
+    });
+
+    // set sidebar closed and body solid layout mode
+    $rootScope.settings.layout.pageContentWhite = true;
+    $rootScope.settings.layout.pageBodySolid = false;
+    $rootScope.settings.layout.pageSidebarClosed = false;
+
+
+    
+});
